1f33240ac032e71112ee252eaad872502bce3cbe
Julian <jlaxmane@sfu.ca>
2018-03-01 13:36:49 -0800

    Added sessions function
    Cleverbot update made sessions mandatory. Session creation on startup still required

--- chat/chat.py
+++ chat/chat.py
@@ -7,9 +7,7 @@ import os
 import requests
 import json
 
-#Global Vars
-user = "user"
-key = "key"
+#will need to implement custom nickname creation
 """Get your own id and key at https://cleverbot.io/keys."""
 
 class chat:
@@ -33,30 +31,42 @@ class chat:
     @chat.command()
     @checks.is_owner()
     async def toggle(self):
-    	"""Turn chatbot on or off"""
-    	self.settings["TOGGLE"] = not self.settings["TOGGLE"]
-    	if self.settings["TOGGLE"]:
-    		await self.bot.say("I will respond to pings.")
-    	else:
-    		await self.bot.say("I won't reply anymore.")
+        """Turn chatbot on or off"""
+        self.settings["TOGGLE"] = not self.settings["TOGGLE"]
+        if self.settings["TOGGLE"]:
+            await self.bot.say("I will respond to pings.")
+        else:
+            await self.bot.say("I won't reply anymore.")
 
     @chat.command()
     @checks.is_owner()
     async def user(self, user: str):
-    	"""Sets user token for cleverbot.io"""
-    	self.settings["chatbot_id"] = user
-    	self.settings.pop("user", None)
-    	dataIO.save_json("data/chat/settings.json", self.settings)
-    	await self.bot.say("User ID set")
+        """Sets user token for cleverbot.io"""
+        self.settings["chatbot_id"] = user
+        self.settings.pop("user", None)
+        dataIO.save_json("data/chat/settings.json", self.settings)
+        await self.bot.say("User ID set")
+
+    @chat.command()
+    @checks.is_owner()
+    async def newsession(self):
+        """Creates or restarts session for chatbot"""
+        user = self.settings["chatbot_id"]
+        key = self.settings["chatbot_api_key"]
+        r=json.loads(requests.post('https://cleverbot.io/1.0/create',json={'user':user,'key':key,'nick':'you'}).text)
+        if r['status'] == 'success':
+            await self.bot.say("New Session created")
+        else:
+            await self.bot.say("Session failed to restart")
 
     @chat.command()
     @checks.is_owner()
     async def apikey(self, key: str):
-    	"""Sets api key for cleverbot.io"""
-    	self.settings["chatbot_api_key"] = key
-    	self.settings.pop("key", None)
-    	dataIO.save_json("data/chat/settings.json", self.settings)
-    	await self.bot.say("API key set")
+        """Sets api key for cleverbot.io"""
+        self.settings["chatbot_api_key"] = key
+        self.settings.pop("key", None)
+        dataIO.save_json("data/chat/settings.json", self.settings)
+        await self.bot.say("API key set")
 
     async def get_response(self, author, text, message):
         """Sends a formatted message to cleverbot and returns a response"""
@@ -64,28 +74,29 @@ class chat:
         key = self.settings["chatbot_api_key"]
         r=json.loads(requests.post('https://cleverbot.io/1.0/ask',json={'user':user,'key':key,'nick':'you', 'text':text}).text)
         if r['status'] == 'success':
-        	return r['response']
+            return r['response']
         else:
-        	return 'Sorry I cannot talk at the moment try again later'
+            r=json.loads(requests.post('https://cleverbot.io/1.0/create',json={'user':user,'key':key,'nick':'you'}).text)
+            return 'Sorry I cannot talk at the moment try again later'
 
     async def on_message(self, message):
-    	"""Takes user input and formats it into a proper message for the bot"""
-    	if not self.settings["TOGGLE"] or message.server is None:
-    		return
+        """Takes user input and formats it into a proper message for the bot"""
+        if not self.settings["TOGGLE"] or message.server is None:
+            return
 
-    	if not self.bot.user_allowed(message):
-    		return
-    	author = message.author
-    	channel = message.channel
-    	if message.author.id != self.bot.user.id:
-    		to_strip = "@" + author.server.me.display_name + " "
-    		text = message.clean_content
-    		if not text.startswith(to_strip):
-    			return
-    		text = text.replace(to_strip, "", 1)
-    		await self.bot.send_typing(channel)
-    		response = await self.get_response(author, text, message)
-    		await self.bot.send_message(channel, response)
+        if not self.bot.user_allowed(message):
+            return
+        author = message.author
+        channel = message.channel
+        if message.author.id != self.bot.user.id:
+            to_strip = "@" + author.server.me.display_name + " "
+            text = message.clean_content
+            if not text.startswith(to_strip):
+                return
+            text = text.replace(to_strip, "", 1)
+            await self.bot.send_typing(channel)
+            response = await self.get_response(author, text, message)
+            await self.bot.send_message(channel, response)
 
 #Data folder for first time
 def check_folders():
@@ -101,7 +112,7 @@ def check_files():
         dataIO.save_json(f, data)
 
 def setup(bot):
-	check_folders()
-	check_files()
-	n = chat(bot)
-	bot.add_cog(n)
+    check_folders()
+    check_files()
+    n = chat(bot)
+    bot.add_cog(n)
